923c8822805d32f7ae342a1bf53d5d548a9a9e68
denisf01 <denis.folvarek@serapion.net>
2023-01-11 20:03:08 +0100

    Small changes on Exercises page

--- src/components/Exercises/Exercise.js
+++ src/components/Exercises/Exercise.js
@@ -1,13 +1,24 @@
 import classes from "./Exercise.module.css";
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Button from "@mui/material/Button";
 
-const MealItem = (props) => {
+const Exercise = (props) => {
   return (
     <li className={classes.exercise}>
       <div>
         <h3>Test</h3>
       </div>
+      <div>
+        <Button>
+          <EditIcon />
+        </Button>
+        <Button>
+          <DeleteIcon color={"action"} />
+        </Button>
+      </div>
     </li>
   );
 };
-export default MealItem;
+export default Exercise;
--- src/components/MainPage/MainBackground.js
+++ src/components/MainPage/MainBackground.js
@@ -1,16 +1,28 @@
 import classes from "./MainBackground.module.css";
 import myImage from "../../images/main-image.jpg";
-import {useContext} from "react";
+import myImage2 from "../../images/main-image2.jpg";
+
+import { useContext } from "react";
 import Context from "../../store/context";
 import CustomizedAlert from "../Alert/Alert";
-
+import ExercisesPhoto from "../../images/exercises.jpg";
 
 const MainBackground = (props) => {
-    const ctx = useContext(Context);
+  const ctx = useContext(Context);
   return (
-    <div className={classes.image}>
-        {ctx.isLoggedOut && <CustomizedAlert text={"Logged out"}/>}
-      <img src={myImage} />
+    <div
+      style={{
+        backgroundImage: `url(${myImage2})`,
+        width: "100%",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+          opacity: "1"
+
+
+      }}
+    >
+      {ctx.isLoggedOut && <CustomizedAlert text={"Logged out"} />}
       <div className={classes.text}>
         <h1>Welcome to the page!</h1>
         <h3>Track all kinds of fitness activities</h3>
